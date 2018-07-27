10f5df168c9cb8fd756af7e5f8599031b100f545
Cheng-Yeh Chen <gary9192631770@gmail.com>
2018-07-27 20:07:47 +0800

    update comments

--- exercise3.py
+++ exercise3.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-###################
+# 可以把腳位定義在這邊
+  
   
 def move_callback(button):
-  print('button {} call back.'.format(button))
-  ################
+  # 這邊要實作callback
     
 def main():
-  ###########################
-    
+  # 這邊要做三件事情
+  # 1. setmode()
+  # 2. setup()
+  # 3. 註冊事件
+  
+  # 這段是要讓程式不終止
   while True:
     time.sleep(1)
   
 if __name__=='__main__':
-main()
+  main()
