b86127c1c4d94422b6d5b8410c81c985f5d1961a
Cheng-Yeh Chen <gary9192631770@gmail.com>
2018-07-27 20:01:30 +0800

    add comments

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,17 @@
+import RPi.GPIO as GPIO
+import time
 
+###################
+  
+def move_callback(button):
+  print('button {} call back.'.format(button))
+  ################
+    
+def main():
+  ###########################
+    
+  while True:
+    time.sleep(1)
+  
+if __name__=='__main__':
+main()
